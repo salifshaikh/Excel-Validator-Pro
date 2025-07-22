5c75b72e0ef439be57df178c598b815c85c1a63a
Salif Shaikh <shaikhsalif50@gmail.com>
2025-07-22 18:11:52 +0530

    title

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>AuditVerse</title>
+        <title>Data Validator</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body className={`${outfit.className} dark:bg-gray-900`}>
